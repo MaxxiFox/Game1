1a7e47a9d3a6dcbd5c38334b9ececf1ec947c461
MaxxiFox <tobi981@gmx.de>
2019-02-19 23:56:11 +0100

    nächstes update

--- main.lua
+++ main.lua
@@ -1,9 +1,13 @@
-
-
 --Alle zu ladenden Elemente sind hier drin
 function love.load()
+     love.window.setTitle("Bloxxs")
+     imageData = love.image.newImageData("graphics/block1.png")
+     love.window.setIcon(imageData)
      block1 = love.graphics.newImage("graphics/block1.png")
 end
+
+
+--Eigene Funktionen beginnen hier
 
 function drawing ()
     for j = 0, 59 do
@@ -19,13 +23,29 @@ function drawing ()
 end
 
 function titletext ()
+    love.graphics.draw(block1, 180, 600/20, 0.7, 6, 6)
     love.graphics.setColor(0.5, 0.6, 0.2)
-    love.graphics.print("Sellorx", 500/2, 600/20, 0, 7, 7)
+    love.graphics.print("Bloxxs", 500/2, 600/20, 0, 7, 7)
     love.graphics.setColor(100, 100, 100)
 end
 
+function helpBox ()
+    helpTitle = "Hilfe"
+    helpMessage = "Aktuell ist keine Hilfe verfügbar"
+    helpButtons = {"OK", escapebutton = 1}
+    love.window.showMessageBox(helpTitle, helpMessage, info, false)
+end
+
+--Eigene Funktionen Enden hier
+
 --Update-Funktion für Bewegungen und immer wiederkehrende Funktionen
 function love.update()
+    if love.keyboard.isDown("escape") then
+        love.event.quit()
+    end
+    if love.keyboard.isDown("f1") then
+        helpBox()
+    end     
 end
 
 --Zeichnen des Spielfeldes
